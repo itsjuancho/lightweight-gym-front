c412517f73e280c4fb026765de6b16496b61aed1
git-chad <tobimocc@gmail.com>
2024-03-25 19:40:47 -0300

    fix(dev): navbar improvements

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return route === "/login" || route === "/register" ? null : (
     <div className="z-50 fixed top-0 h-28 min-w-[100dvw] aeonik text-gray-50 text-2xl py-8">
-      <Container className="px-20 flex justify-between items-center items-start">
+      <Container className="px-20 flex justify-between items-center">
         <a href="/" className="flex items-center">
           <Image src={lwLogo} />
           <p className="coanda-bold ml-6">Lightweight</p>
@@ -19,17 +19,39 @@ const Navbar = () => {
 
         <ul className="flex space-x-4 text-lg">
           <li>
-            <Link href={"/products"}>Products</Link>
+            <Link
+              href="/products"
+              className="group transition duration-300 animate-fade-in"
+            >
+              Products
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-[1px] bg-white"></span>
+            </Link>
           </li>
           <li>
-            <Link href="/about-us">About us</Link>
+            <Link
+              href="/about-us"
+              className="group transition duration-300 animate-fade-in"
+            >
+              About us
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-[1px] bg-white"></span>
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <Link
+              href="/contact"
+              className="group transition duration-300 animate-fade-in"
+            >
+              Contact
+              <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-[1px] bg-white"></span>
+            </Link>
           </li>
-          <li className="text-red-500">
-            <Link href="/login">Join/ sign in</Link>
-          </li>
+          <Link
+            href="/login"
+            className="group transition duration-300 animate-fade-in text-red-500"
+          >
+            Join/ sign in
+            <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-[1px] bg-red-500"></span>
+          </Link>
         </ul>
       </Container>
     </div>
