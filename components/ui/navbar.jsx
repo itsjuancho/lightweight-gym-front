1e9d58e9b3c6ab74cae7ee499ce22c1419451192
Elias Vasquez <gamergy83@gmail.com>
2024-03-25 20:19:50 -0500

    fix navbar

--- components/ui/navbar.jsx
+++ components/ui/navbar.jsx
@@ -1,24 +1,54 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import lwLogo from "../../public/lw-logo.svg";
 import Container from "./container";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {LOGIN_URL, ROUTE_CART, ROUTE_LOGIN, ROUTE_REGISTER} from '../../app/utils/routes';
+import {
+  LOGIN_URL,
+  ROUTE_CART,
+  ROUTE_LOGIN,
+  ROUTE_REGISTER,
+} from "../../app/utils/routes";
 
 const Navbar = () => {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+
+    setVisible(
+      (prevScrollPos > currentScrollPos &&
+        prevScrollPos - currentScrollPos > 70) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
   const route = usePathname();
-  
-  return route !== "/"? null : (
-    <div className="z-50 fixed top-0 h-28 min-w-[100dvw] aeonik text-gray-50 text-2xl py-8">
+
+  return route !== "/" ? null : (
+    <div
+      className={`z-50 fixed top-0 h-28 min-w-[100dvw] aeonik text-gray-50 text-2xl py-8 ${
+        visible ? "visible" : "hidden"
+      }`}
+    >
       <Container className="px-20 flex justify-between items-start">
         <a href="/" className="flex items-center">
           <Image src={lwLogo} alt="logo" />
           <p className="coanda-bold ml-6">Lightweight</p>
         </a>
 
-        <ul>
+        <ul className="flex justify-around w-[40%]">
           <li className="text-red-500">
             <Link href={ROUTE_LOGIN}>Join/</Link>
             <Link href={ROUTE_REGISTER}> sign in</Link>
