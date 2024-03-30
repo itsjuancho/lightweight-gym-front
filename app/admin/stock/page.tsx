f4c09b7d062c08c4f59e9e5e172beca570267b33
git-chad <tobimocc@gmail.com>
2024-03-29 23:15:09 -0300

    ...

---
+++ app/admin/stock/page.tsx
@@ -0,0 +1,13 @@
+import Container from "../../../components/ui/container"
+
+const ReportsPage = () => {
+    return (
+        <div className="min-h-[100dvh] bg-gradient-to-b from-[#030712] to-[#210303] text-gray-50  flex flex-col justify-center">
+            <Container className="pt-40 pb-16 px-20">
+                <h1>Stock management</h1>
+            </Container>
+        </div>
+    )
+}
+
+export default ReportsPage
